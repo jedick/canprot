2b50492c663347e4826bcf8f3ad1c35778e98c37
jedick <j3ffdick@gmail.com>
2016-09-14 09:13:05 +0700

    More flexible specification of pdat_fun

--- R/get_pdat.R
+++ R/get_pdat.R
@@ -2,21 +2,17 @@
 # get protein expression and composition data
 # 20160712 jmd
 
-get_pdat <- function(dataset=NULL, pdat_fun=NULL, basis="AA") {
-  # the names of pdat_* functions in canprot namespace
-  pdat1 <- "pdat_CRC"
-  # names of any additional functions requested by the user
-  pdat <- c(pdat1, pdat_fun)
+get_pdat <- function(dataset=NULL, pdat_fun="pdat_CRC", basis="AA") {
   # datasets available from all functions
-  datasets <- lapply(pdat, function(x) get(x)())
-  names(datasets) <- pdat
+  datasets <- lapply(pdat_fun, function(x) get(x)())
+  names(datasets) <- pdat_fun
   if(is.null(dataset)) return(datasets)
   # which function (if any) provides this dataset
   matchdat <- lapply(datasets, function(x) match(dataset, x))
   hasdat <- sapply(matchdat, function(x) !is.na(x))
   if(sum(hasdat) < 1) stop(paste("no function provides data for", dataset))
-  if(sum(hasdat) > 1) stop(paste0("multiple functions (", paste(pdat[hasdat], collapse=", "), ") provide data for ", dataset))
+  if(sum(hasdat) > 1) stop(paste0("multiple functions (", paste(pdat_fun[hasdat], collapse=", "), ") provide data for ", dataset))
   # in case the name of the dataset has an =XX suffix, remove it here
   dataset <- strsplit(dataset, "=")[[1]][1]
-  return(get(pdat[hasdat])(dataset))
+  return(get(pdat_fun[hasdat])(dataset, basis))
 }
