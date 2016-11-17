9749d94f6c356bb839e7b09b24a8f438028f54a1
jedick <j3ffdick@gmail.com>
2016-11-17 18:18:15 +0700

    Begin adding more data: CRC

--- R/pdat_CRC.R
+++ R/pdat_CRC.R
@@ -1,35 +1,38 @@
 # canprot/R/pdat_CRC.R
-# get protein data for CRC
+# get protein data for colorectal cancer
 # 20160703 jmd
+# 20161011 updated with new data [LXM+16] and =AD (adenoma as n2) flags
 
 pdat_CRC <- function(dataset=NULL, basis="AA") {
   # list available datasets
   if(is.null(dataset)) { 
-    return(c("WTK+08", "AKP+10_CRC", "AKP+10_CIN", "AKP+10_MIN", "JKMF10",
-             "XZC+10_I", "XZC+10_II", "ZYS+10", "BPV+11_adenoma",
-             "BPV+11_stage.I", "BPV+11_stage.II", "BPV+11_stage.III",
-             "BPV+11_stage.IV", "JCF+11", "MRK+11_AD.NC", "MRK+11_AC.AD",
-             "MRK+11_AC.NC", "KKL+12", "KYK+12", "WOD+12", "YLZ+12", "MCZ+13",
-             "KWA+14", "UNS+14", "WKP+14", "STK+15", "WDO+15_A.N",
-             "WDO+15_C.A", "WDO+15_C.N", "LPL+16_ACP", "LPL+16_CIS",
-             "LPL+16_ICC", "PHL+16_AD", "PHL+16_CIS", "PHL+16_ICC"))
+    return(c("WTK+08",
+             "AKP+10_CRC", "AKP+10_CIN", "AKP+10_MIN", "JKMF10", "XZC+10_I", "XZC+10_II", "ZYS+10",
+             "BPV+11_adenoma=AD", "BPV+11_stage.I", "BPV+11_stage.II", "BPV+11_stage.III", "BPV+11_stage.IV",
+             "JCF+11", "MRK+11_AD.NC=AD", "MRK+11_AC.AD", "MRK+11_AC.NC",
+             "KKL+12", "KYK+12", "WOD+12", "YLZ+12",
+             "MCZ+13",
+             "KWA+14", "UNS+14=AD", "WKP+14",
+             "STK+15", "WDO+15_A.N=AD", "WDO+15_C.A", "WDO+15_C.N",
+             "LPL+16_ACP=AD", "LPL+16_CIS", "LPL+16_ICC", "LXM+16",
+             "PHL+16_AD=AD", "PHL+16_CIS", "PHL+16_ICC"))
   }
   # get study and stage/condition
   study <- strsplit(dataset, "_")[[1]][1]
-  stage <- strsplit(dataset, "_")[[1]][2]
-  extdatadir <- paste0(system.file("extdata", package="canprot"), "/")
+  stage <- paste(strsplit(dataset, "_")[[1]][-1], collapse="_")
+  datadir <- paste0(system.file("extdata", package="canprot"), "/expression/CRC/")
   if(study=="JKMF10") {
     # 20150520 up- and down-regulated CRC-associated proteins reported in 4 or more studies, from Jimenez et al., 2010
     description <- "serum biomarkers up / down"
     print(paste0("pdat.CRC: ", description, " [", dataset, "]"))
-    dat <- read.csv(paste0(extdatadir, "JKMF10.csv"), as.is=TRUE)
+    dat <- read.csv(paste0(datadir, "JKMF10.csv"), as.is=TRUE)
     # get compositional features
     pcomp <- protcomp(dat$Uniprot.ID, basis=basis)
     up2 <- dat$Change=="UP"
     names <- dat$Gene.name
   } else if(study=="KWA+14") {
     # 20150908 chromatin-binding fraction, Knol et al., 2014
-    dat <- read.csv(paste0(extdatadir, "KWA+14.csv"), as.is=TRUE)
+    dat <- read.csv(paste0(datadir, "KWA+14.csv"), as.is=TRUE)
     description <- "chromatin-binding C / A"
     print(paste0("pdat.CRC: ", description, " [", dataset, "]"))
     pcomp <- protcomp(dat$Entry, basis=basis)
@@ -38,7 +41,7 @@ pdat_CRC <- function(dataset=NULL, basis="AA") {
     names <- dat$Symbol
   } else if(study=="STK+15") {
     # 20151004 CRC membrane-enriched proteome, Sethi et al., 2015
-    dat <- read.csv(paste0(extdatadir, "STK+15.csv"), as.is=TRUE)
+    dat <- read.csv(paste0(datadir, "STK+15.csv"), as.is=TRUE)
     description <- "membrane enriched T / N"
     print(paste0("pdat.CRC: ", description, " [", dataset, "]"))
     # drop uncharacterized proteins
@@ -50,7 +53,7 @@ pdat_CRC <- function(dataset=NULL, basis="AA") {
     names <- dat$gene
   } else if(study=="UNS+14") {
     # 20151005 epithelial cell signature, Uzozie et al., 2014
-    dat <- read.csv(paste0(extdatadir, "UNS+14.csv"), as.is=TRUE)
+    dat <- read.csv(paste0(datadir, "UNS+14.csv"), as.is=TRUE)
     description <- "epithelial adenoma / normal"
     print(paste0("pdat.CRC: ", description, " [", dataset, "]"))
     # remove duplicated IDs
@@ -61,7 +64,7 @@ pdat_CRC <- function(dataset=NULL, basis="AA") {
   } else if(study=="BPV+11") {
     # 20160414 CRC Besson et al., 2015
     # BPV+11_adenoma, BPV+11_stage.I, BPV+11_stage.II, BPV+11_stage.III, BPV+11_stage.IV
-    dat <- read.csv(paste0(extdatadir, "BPV+11.csv"), as.is=TRUE)
+    dat <- read.csv(paste0(datadir, "BPV+11.csv"), as.is=TRUE)
     description <- paste0(gsub("\\.", " ", stage), " / normal")
     print(paste0("pdat.CRC: ", description, " [", dataset, "]"))
     # keep signifcantly changed proteins for the cancer stage
@@ -72,7 +75,7 @@ pdat_CRC <- function(dataset=NULL, basis="AA") {
   } else if(study=="WDO+15") {
     # 20160414 Wiśniewski et al., 2015
     # WDO+15_A.N, WDO+15_C.A, WDO+15_C.N
-    dat <- read.csv(paste0(extdatadir, "WDO+15.csv"), as.is=TRUE)
+    dat <- read.csv(paste0(datadir, "WDO+15.csv"), as.is=TRUE)
     if(stage=="A.N") description <- "adenoma / normal"
     if(stage=="C.A") description <- "carcinoma / adenoma"
     if(stage=="C.N") description <- "carcinoma / normal"
@@ -94,7 +97,7 @@ pdat_CRC <- function(dataset=NULL, basis="AA") {
     up2 <- dat[, irat] > 0
   } else if(study=="WOD+12") {
     # 20160418 CRC tumor tissue, Wiśniewski et al., 2012
-    dat <- read.csv(paste0(extdatadir, "WOD+12.csv"), as.is=TRUE)
+    dat <- read.csv(paste0(datadir, "WOD+12.csv"), as.is=TRUE)
     description <- "T / N"
     print(paste0("pdat.CRC: ", description, " [", dataset, "]"))
     # drop missing UniProt IDs
@@ -110,7 +113,7 @@ pdat_CRC <- function(dataset=NULL, basis="AA") {
     up2 <- dat$Median.Ratio.C.N > 1
   } else if(study=="JCF+11") {
     # 20160422 tumor vs normal, Jankova et al., 2011
-    dat <- read.csv(paste0(extdatadir, "JCF+11.csv"), as.is=TRUE)
+    dat <- read.csv(paste0(datadir, "JCF+11.csv"), as.is=TRUE)
     description <- "T / N"
     print(paste0("pdat.CRC: ", description, " [", dataset, "]"))
     pcomp <- protcomp(dat$Accession, basis=basis)
@@ -119,7 +122,7 @@ pdat_CRC <- function(dataset=NULL, basis="AA") {
   } else if(study=="XZC+10") {
     # 20160426 stage I and II vs normal, Xie et al., 2010
     # XZC+10_I, XZC+10_II
-    dat <- read.csv(paste0(extdatadir, "XZC+10.csv"), as.is=TRUE)
+    dat <- read.csv(paste0(datadir, "XZC+10.csv"), as.is=TRUE)
     description <- paste("stage", stage, "/ normal")
     print(paste0("pdat.CRC: ", description, " [", dataset, "]"))
     # use data for the specified stage
@@ -140,7 +143,7 @@ pdat_CRC <- function(dataset=NULL, basis="AA") {
   } else if(study=="AKP+10") {
     # 20160427 adenoma ADE vs CRC, CIN, MIN, Albrethsen et al., 2010
     # AKP+10_CRC, AKP+10_CIN, AKP+10_MIN
-    dat <- read.csv(paste0(extdatadir, "AKP+10.csv"), as.is=TRUE)
+    dat <- read.csv(paste0(datadir, "AKP+10.csv"), as.is=TRUE)
     description <- paste(stage, "nuclear matrix C / A")
     print(paste0("pdat.CRC: ", description, " [", dataset, "]"))
     # use the specified data set
@@ -153,7 +156,7 @@ pdat_CRC <- function(dataset=NULL, basis="AA") {
     names <- dat$Gene.Symbol
   } else if(study=="KKL+12") {
     # 20160428 poor / good prognosis, Kim et al., 2012
-    dat <- read.csv(paste0(extdatadir, "KKL+12.csv"), as.is=TRUE)
+    dat <- read.csv(paste0(datadir, "KKL+12.csv"), as.is=TRUE)
     description <- "poor / good prognosis"
     print(paste0("pdat.CRC: ", description, " [", dataset, "]"))
     # drop missing proteins
@@ -169,7 +172,7 @@ pdat_CRC <- function(dataset=NULL, basis="AA") {
     up2 <- dat$protein.ratio..G.P. < 1
   } else if(study=="WKP+14") {
     # 20160428 tissue secretome, de Wit et al., 2014
-    dat <- read.csv(paste0(extdatadir, "WKP+14.csv"), as.is=TRUE)
+    dat <- read.csv(paste0(datadir, "WKP+14.csv"), as.is=TRUE)
     description <- "tissue secretome T / N"
     print(paste0("pdat.CRC: ", description, " [", dataset, "]"))
     pcomp <- protcomp(dat$Entry, basis=basis)
@@ -177,7 +180,7 @@ pdat_CRC <- function(dataset=NULL, basis="AA") {
     names <- dat$Gene.Symbol
   } else if(study=="KYK+12") {
     # 20160428 MSS-type CRC, Kang et al., 2012
-    dat <- read.csv(paste0(extdatadir, "KYK+12.csv"), as.is=TRUE)
+    dat <- read.csv(paste0(datadir, "KYK+12.csv"), as.is=TRUE)
     description <- "MSS-type T / N"
     print(paste0("pdat.CRC: ", description, " [", dataset, "]"))
     pcomp <- protcomp(dat$Entry, basis=basis)
@@ -185,7 +188,7 @@ pdat_CRC <- function(dataset=NULL, basis="AA") {
     names <- dat$gene.name
   } else if(study=="ZYS+10") {
     # 20160430 microdissected T / N, Zhang et al., 2010
-    dat <- read.csv(paste0(extdatadir, "ZYS+10.csv"), as.is=TRUE)
+    dat <- read.csv(paste0(datadir, "ZYS+10.csv"), as.is=TRUE)
     description <- "microdissected T / N"
     print(paste0("pdat.CRC: ", description, " [", dataset, "]"))
     # drop missing proteins
@@ -202,7 +205,7 @@ pdat_CRC <- function(dataset=NULL, basis="AA") {
   } else if(study=="MRK+11") {
     # 20160509 T / N, Mikula et al., 2011
     # MRK+11_AD.NC, MRK+11_AC.AD, MRK+11_AC.NC
-    dat <- read.csv(paste0(extdatadir, "MRK+11.csv"), as.is=TRUE)
+    dat <- read.csv(paste0(datadir, "MRK+11.csv"), as.is=TRUE)
     if(stage=="AD.NC") description <- "adenoma / normal"
     if(stage=="AC.AD") description <- "adenocarcinoma / adenoma"
     if(stage=="AC.NC") description <- "adenocarcinoma / normal"
@@ -225,7 +228,7 @@ pdat_CRC <- function(dataset=NULL, basis="AA") {
     up2 <- dat[, iFC] > 1
   } else if(study=="YLZ+12") {
     # 20160511 conditioned media T / N, Yao et al., 2012
-    dat <- read.csv(paste0(extdatadir, "YLZ+12.csv"), as.is=TRUE)
+    dat <- read.csv(paste0(datadir, "YLZ+12.csv"), as.is=TRUE)
     description <- "conditioned media T / N"
     print(paste0("pdat.CRC: ", description, " [", dataset, "]"))
     # list known UniProt IDs
@@ -237,7 +240,7 @@ pdat_CRC <- function(dataset=NULL, basis="AA") {
     up2 <- dat$Rsca > 0
   } else if(study=="WTK+08") {
     # 20160511 T / N, Watanabe et al., 2008
-    dat <- read.csv(paste0(extdatadir, "WTK+08.csv"), as.is=TRUE)
+    dat <- read.csv(paste0(datadir, "WTK+08.csv"), as.is=TRUE)
     description <- "T / N"
     print(paste0("pdat.CRC: ", description, " [", dataset, "]"))
     # list known UniProt IDs
@@ -252,7 +255,7 @@ pdat_CRC <- function(dataset=NULL, basis="AA") {
   } else if(study=="PHL+16") {
     # 20160602 AD/NC, CIS/NC, ICC/NC, Peng et al., 2016
     # PHL+16_AD, PHL+16_CIS, PHL+16_ICC
-    dat <- read.csv(paste0(extdatadir, "PHL+16.csv"), as.is=TRUE)
+    dat <- read.csv(paste0(datadir, "PHL+16.csv"), as.is=TRUE)
     if(stage=="AD") description <- "AD / NC"
     if(stage=="CIS") description <- "CIS / NC"
     if(stage=="ICC") description <- "ICC / NC"
@@ -271,7 +274,7 @@ pdat_CRC <- function(dataset=NULL, basis="AA") {
   } else if(study=="LPL+16") {
     # 20160602 stromal ACP/NNCM, CIS/NNCM, ICC/NNCM, Li et al., 2016
     # LPL+16_ACP, LPL+16_CIS, LPL+16_ICC
-    dat <- read.csv(paste0(extdatadir, "LPL+16.csv"), as.is=TRUE)
+    dat <- read.csv(paste0(datadir, "LPL+16.csv"), as.is=TRUE)
     if(stage=="ACP") description <- "stromal AD / NC"
     if(stage=="CIS") description <- "stromal CIS / NC"
     if(stage=="ICC") description <- "stromal ICC / NC"
@@ -283,7 +286,7 @@ pdat_CRC <- function(dataset=NULL, basis="AA") {
     up2 <- dat[, icol] > 1.5
   } else if(study=="MCZ+13") {
     # 20160602 stromal T/N, Mu et al., 2013
-    dat <- read.csv(paste0(extdatadir, "MCZ+13.csv"), as.is=TRUE)
+    dat <- read.csv(paste0(datadir, "MCZ+13.csv"), as.is=TRUE)
     description <- "stromal T / N"
     print(paste0("pdat.CRC: ", description, " [", dataset, "]"))
     # drop missing proteins and duplicated proteins
@@ -291,6 +294,22 @@ pdat_CRC <- function(dataset=NULL, basis="AA") {
     dat <- remove_entries(dat, duplicated(dat$Entry), dataset, "duplicated")
     pcomp <- protcomp(dat$Entry, basis=basis)
     up2 <- dat$CS.vs..NS > 0
+  } else if(study=="LXM+16") {
+    # 20160728 CRC, Liu et al., 2016
+    dat <- read.csv(paste0(datadir, "LXM+16.csv"), as.is=TRUE)
+    description <- "biopsy T / N"
+    print(paste0("pdat_hypoxia: ", description, " [", dataset, "]"))
+    # drop missing proteins
+    dat <- remove_entries(dat, is.na(dat$UniProt), dataset, "missing")
+    # list the known UniProt IDs and take the first (non-NA) match
+    knownIDs <- check_ID(dat$UniProt)
+    ID <- sapply(sapply(knownIDs, na.omit), "[", 1)
+    dat$UniProt <- ID
+    # drop unavailable and duplicated proteins
+    dat <- remove_entries(dat, is.na(dat$UniProt), dataset, "unavailable")
+    dat <- remove_entries(dat, duplicated(dat$UniProt), dataset, "duplicated")
+    pcomp <- protcomp(dat$UniProt, basis=basis)
+    up2 <- dat$Ratio..C.N. > 1
   } else stop(paste("CRC dataset", dataset, "not available"))
   return(list(dataset=dataset, basis=basis, description=description, pcomp=pcomp, up2=up2, names=names))
 }
