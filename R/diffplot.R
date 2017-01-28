bc13ff879d80fba7dfeaa665c240882c693af019
jedick <j3ffdick@gmail.com>
2017-01-28 14:56:00 +0700

    Add function: CNS()

--- R/diffplot.R
+++ R/diffplot.R
@@ -1,46 +1,53 @@
 # canprot/R/diffplot.R
-# plot differences in means of ZC and nH2O
-# and indicate low p-values with lines
+# plot mean differences of ZC and nH2O, or other variables
 # 20160715 jmd
 
-diffplot <- function(comptab, col="black", plot.rect=FALSE, plot.text=TRUE) {
+diffplot <- function(comptab, vars=c("ZC", "nH2O"), col="black", plot.rect=FALSE, plot.text=TRUE) {
   # convert to data frame if needed
   if(!is.data.frame(comptab)) comptab <- do.call(rbind, comptab)
+  # which columns we're using
+  stats <- c("diff", "CLES", "p.value")
+  iX <- sapply(paste(vars[1], stats, sep="."), grep, colnames(comptab))
+  iY <- sapply(paste(vars[2], stats, sep="."), grep, colnames(comptab))
   # get mean difference, common language effect size and p-value
-  ZC_d <- comptab$ZC.diff
-  ZC_e <- signif(comptab$ZC.CLES, 2)
-  ZC_p <- comptab$ZC.p.value
-  nH2O_d <- comptab$nH2O.diff
-  nH2O_e <- signif(comptab$nH2O.CLES, 2)
-  nH2O_p <- comptab$nH2O.p.value
+  X_d <- comptab[, iX[1]]
+  X_e <- signif(comptab[, iX[2]], 2)
+  X_p <- comptab[, iX[3]]
+  Y_d <- comptab[, iY[1]]
+  Y_e <- signif(comptab[, iY[2]], 2)
+  Y_p <- comptab[, iY[3]]
   # set up plot
-  xlab <- substitute("mean difference (" * x * ")", list(x=cplab$ZC[[1]]))
-  ylab <- substitute("mean difference (" * y * ")", list(y=cplab$nH2O[[1]]))
+  x <- vars[1]
+  y <- vars[2]
+  if(vars[1]=="ZC") x <- cplab$ZC[[1]]
+  if(vars[2]=="nH2O") y <- cplab$nH2O[[1]]
+  xlab <- substitute("mean difference (" * x * ")", list(x=x))
+  ylab <- substitute("mean difference (" * y * ")", list(y=y))
   # initialize plot: add a 0 to make sure we can see the axis
-  plot(type="n", c(ZC_d, 0), c(nH2O_d, 0), xlab=xlab, ylab=ylab)
+  plot(type="n", c(X_d, 0), c(Y_d, 0), xlab=xlab, ylab=ylab)
   # add a reference rectangle
   if(plot.rect) rect(-0.01, -0.01, 0.01, 0.01, col="grey80", lwd=0)
   # show axis lines
   abline(h=0, lty=3)
   abline(v=0, lty=3)
   # show drop lines: dotted/solid if p-value/effect size meet criteria
-  lty.ZC <- ifelse(abs(ZC_e - 50) >= 10, 1, ifelse(ZC_p < 0.05, 2, 0))
-  lty.nH2O <- ifelse(abs(nH2O_e - 50) >= 10, 1, ifelse(nH2O_p < 0.05, 2, 0))
-  for(i in seq_along(ZC_d)) {
-    lines(rep(ZC_d[i], 2), c(0, nH2O_d[i]), lty=lty.ZC[i])
-    lines(c(0, ZC_d[i]), rep(nH2O_d[i], 2), lty=lty.nH2O[i])
+  lty.X <- ifelse(abs(X_e - 50) >= 10, 1, ifelse(X_p < 0.05, 2, 0))
+  lty.Y <- ifelse(abs(Y_e - 50) >= 10, 1, ifelse(Y_p < 0.05, 2, 0))
+  for(i in seq_along(X_d)) {
+    lines(rep(X_d[i], 2), c(0, Y_d[i]), lty=lty.X[i])
+    lines(c(0, X_d[i]), rep(Y_d[i], 2), lty=lty.Y[i])
   } 
   # point symbols: open circle, filled circle, filled square (0, 1 or 2 vars with p-value < 0.05)
-  p_signif <- rowSums(data.frame(ZC_p < 0.05, nH2O_p < 0.05))
+  p_signif <- rowSums(data.frame(X_p < 0.05, Y_p < 0.05))
   pch <- ifelse(p_signif==2, 15, ifelse(p_signif==1, 19, 21))
   # plot points with specified color
   col <- rep(col, length.out=nrow(comptab))
-  if(!plot.text) points(ZC_d, nH2O_d, pch=pch, col=col, bg="white")
+  if(!plot.text) points(X_d, Y_d, pch=pch, col=col, bg="white")
   else {
     # plot bigger points with letters inside
-    points(ZC_d, nH2O_d, pch=pch, col=col, bg="white", cex=2)
+    points(X_d, Y_d, pch=pch, col=col, bg="white", cex=2)
     # use white letters on colored background
     col[pch %in% c(15, 19)] <- "white"
-    text(ZC_d, nH2O_d, c(letters, LETTERS)[seq_along(ZC_d)], col=col, cex=0.9)
+    text(X_d, Y_d, c(letters, LETTERS)[seq_along(X_d)], col=col, cex=0.9)
   }
 }
