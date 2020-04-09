609d9332697c3ef3e25a70c8eaece447768bbc76
jedick <j3ffdick@gmail.com>
2020-04-10 07:56:18 +0800

    Refresh CSV files saved from vignettes

--- R/pdat_osmotic.R
+++ R/pdat_osmotic.R
@@ -21,7 +21,7 @@ pdat_osmotic <- function(dataset = 2020, basis = "rQEC") {
              "IXA+19=glucose", "MGF+19_10=microbial", "MGF+19_20=microbial",
              "AST+20=microbial",
              "MHP+20_H9c2=glucose", "MHP+20_HEK=glucose",
-             "MPR+20_3h.high.glucose=glucose", "MPR+20_12h.high.glucose=glucose", "MPR+20_3h.high.mannitol", "MPR+20_24h.high.mannitol"
+             "MPR+20_3h.high.glucose=glucose", "MPR+20_24h.high.glucose=glucose", "MPR+20_3h.high.mannitol", "MPR+20_24h.high.mannitol"
              ))
   }
   if(identical(dataset, 2017)) {
@@ -236,7 +236,7 @@ pdat_osmotic <- function(dataset = 2020, basis = "rQEC") {
     pcomp <- protcomp(dat$Accession, basis, aa_file = paste0(extdatadir, "/aa/mouse/SMS+18_aa.csv.xz"))
   } else if(study=="MPR+20") {
     # 20200407 human aortic endothelial cells, Madonna et al., 2020
-    # MPR+20_normal.glucose.insulin, MPR+20_3h.high.glucose, MPR+20_12h.high.glucose, MPR+20_3h.high.mannitol, MPR+20_24h.high.mannitol
+    # MPR+20_normal.glucose.insulin, MPR+20_3h.high.glucose, MPR+20_24h.high.glucose, MPR+20_3h.high.mannitol, MPR+20_24h.high.mannitol
     dat <- read.csv(paste0(datadir, "MPR+20.csv.xz"), as.is=TRUE)
     description <- paste("human aortic endothelial cells in", stage)
     icol <- grep(stage, colnames(dat))
