377185dbf911ddaac715d110853efeb0b1f7ee7d
jedick <j3ffdick@gmail.com>
2020-04-14 20:10:02 +0800

    Rename pdat_recomp() to recomp()

--- R/recomp.R
+++ R/recomp.R
@@ -1,5 +1,5 @@
 # recompute a pdat object for a different 'basis' 20191207
-pdat_recomp <- function(pdat, basis = "rQEC") {
+recomp <- function(pdat, basis = "rQEC") {
   uniprot <- pdat$pcomp$uniprot
   aa <- pdat$pcomp$aa
   pcomp <- protcomp(uniprot, basis, aa)
