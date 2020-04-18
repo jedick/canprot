658a50e09214d7e4db0fbf742e88c9369f874d1e
jedick <j3ffdick@gmail.com>
2020-04-18 12:34:04 +0800

    Extract pdat_osmotic_euk() from pdat_osmotic()

--- R/pdat_osmotic.R
+++ R/pdat_osmotic.R
@@ -7,14 +7,12 @@ pdat_osmotic <- function(dataset = 2020, basis = "rQEC") {
     return(c(
              "PNWB09=microbial",
              "FTR+10=microbial",
-             "LTH+11=microbial", "OBBH11",
              "KKG+12_25C_aw0.985=microbial", "KKG+12_14C_aw0.985=microbial", "KKG+12_25C_aw0.967=microbial", "KKG+12_14C_aw0.967=microbial",
-             "LFY+12_C1h", "LFY+12_C8h", "LFY+12_C2p", "LFY+12_N1h", "LFY+12_N8h", "LFY+12_N2p",
              "LPK+13=microbial", "QHT+13_24.h=microbial", "QHT+13_48.h=microbial",
-             "CLG+15", "KLB+15_prot-suc=microbial", "KLB+15_prot-NaCl=microbial", "YDZ+15=microbial",
+             "KLB+15_prot-suc=microbial", "KLB+15_prot-NaCl=microbial",
              "DSNM16_131C=microbial", "DSNM16_310F=microbial", "RBP+16=microbial",
              "KAK+17=microbial", "LYS+17=microbial",
-             "JBG+18=microbial", "KSK+18", "LJC+18_wt=microbial", "LJC+18_mutant=microbial", "SMS+18_wt", "SMS+18_FGFR12.deficient",
+             "KSK+18", "LJC+18_wt=microbial", "LJC+18_mutant=microbial",
              "LWS+19=microbial", "MGF+19_10=microbial", "MGF+19_20=microbial",
              "AST+20=microbial", "GBR+20_CIRM129=microbial", "GBR+20_CIRM1025=microbial"
              ))
@@ -52,28 +50,6 @@ pdat_osmotic <- function(dataset = 2020, basis = "rQEC") {
     up2 <- dat[, icol] > 0
     dat <- cleanup(dat, "Entry", up2)
     pcomp <- protcomp(dat$Entry, basis=basis, aa_file=paste0(extdatadir, "/aa/bacteria/KKG+12_aa.csv.xz"))
-  } else if(study=="CLG+15") {
-    # 20160925 conjunctival epithelial cells, Chen et al., 2015
-    dat <- read.csv(paste0(datadir, "CLG+15.csv.xz"), as.is=TRUE)
-    description <- paste("human conjunctival epithelial cells in 380 or 480 mOsm vs 280 mOsm NaCl")
-    # use proteins that have same direction of change in both conditions
-    dat <- dat[(dat$T1 > 1 & dat$T2 > 1) | (dat$T1 < 1 & dat$T2 < 1), ]
-    pcomp <- protcomp(dat$accession..UniProtKB.Swiss.Prot., basis=basis)
-    up2 <- dat$T1 > 1
-  } else if(study=="OBBH11") {
-    # 20160925 adipose-derived stem cells, Oswald et al., 2011
-    dat <- read.csv(paste0(datadir, "OBBH11.csv.xz"), as.is=TRUE)
-    description <- "adipose-derived stem cells in 400 mOsm vs 300 mOsm NaCl"
-    dat <- check_IDs(dat, "Uniprot.Protein.Code")
-    pcomp <- protcomp(dat$Uniprot.Protein.Code, basis=basis)
-    up2 <- dat$Elucidator.Expression.Ratio..Treated.Control. > 1
-  } else if(study=="YDZ+15") {
-    # 20160926 Yarrowia lipolytica, Yang et al., 2015
-    dat <- read.csv(paste0(datadir, "YDZ+15.csv.xz"), as.is=TRUE)
-    description <- paste("Yarrowia lipolytica in 4.21 osmol/kg vs 3.17 osmol/kg NaCl")
-    up2 <- dat$Av..ratio..high.low. > 0
-    dat <- cleanup(dat, "Accession.No.", up2)
-    pcomp <- protcomp(substr(dat$Accession.No., 4, 12), basis=basis, aa_file=paste0(extdatadir, "/aa/yeast/YDZ+15_aa.csv.xz"))
   } else if(study=="KLB+15") {
     # 20160926 Caulobacter crescentus, Kohler et al., 2015
     # KLB+15_trans-suc, KLB+15_trans-NaCl, KLB+15_prot-suc, KLB+15_prot-NaCl
@@ -141,37 +117,12 @@ pdat_osmotic <- function(dataset = 2020, basis = "rQEC") {
     dat <- dat[idiff, ]
     up2 <- dat[, icol] > 2
     pcomp <- protcomp(dat$Entry, basis=basis, aa_file = paste0(extdatadir, "/aa/bacteria/MGF+19_aa.csv.xz"))
-  } else if(study=="JBG+18") {
-    # 20200406 Candida albicans 1 M NaCl, Jacobsen et al., 2018
-    dat <- read.csv(paste0(datadir, "JBG+18.csv.xz"), as.is=TRUE)
-    description <- "Candida albicans in 1 M NaCl vs medium with no added NaCl"
-    up2 <- dat$log2.ratio > 0
-    dat <- cleanup(dat, "Entry", up2)
-    pcomp <- protcomp(dat$Entry, basis = basis, aa_file = paste0(extdatadir, "/aa/yeast/JBG+18_aa.csv.xz"))
-  } else if(study=="LTH+11") {
-    # 20200406 Saccharomyces cerevisae 0.7 M NaCl, Lee et al., 2011
-    dat <- read.csv(paste0(datadir, "LTH+11.csv.xz"), as.is=TRUE)
-    description <- "S. cerevisae in 0.7 M NaCl vs control medium (YPD)"
-    up2 <- dat$Regulation == "up"
-    pcomp <- protcomp(dat$Entry, basis, aa_file = paste0(extdatadir, "/aa/yeast/LTH+11_aa.csv.xz"))
   } else if(study=="AST+20") {
     # 20200407 Lactobacillus fermentum, Ali et al., 2020
     dat <- read.csv(paste0(datadir, "AST+20.csv.xz"), as.is=TRUE)
     description <- "Lactobacillus fermentum with vs without 0.3% to 1.5% w/v bile salts"
     up2 <- dat$Folds.change > 1
     pcomp <- protcomp(dat$Protein.IDs, basis, aa_file = paste0(extdatadir, "/aa/bacteria/AST+20_aa.csv.xz"))
-  } else if(study=="SMS+18") {
-    # 20200407 mouse skin in low/high humidity, Seltmann et al., 2018
-    # SMS+18_wt, SMS+18_FGFR12.deficient
-    dat <- read.csv(paste0(datadir, "SMS+18.csv.xz"), as.is=TRUE)
-    description <- paste("epidermal lysate of mice kept in 40% vs 70% humidity,", stage)
-    icol <- grep(stage, colnames(dat))
-    dat <- dat[abs(dat[, icol]) > 0.5, ]
-    dat <- check_IDs(dat, "Accession", aa_file = paste0(extdatadir, "/aa/mouse/SMS+18_aa.csv.xz"))
-    # abundance ratios are given as high humidity / low humidity, make up-expressed be low
-    up2 <- dat[, icol] < 0
-    dat <- cleanup(dat, "Accession", up2)
-    pcomp <- protcomp(dat$Accession, basis, aa_file = paste0(extdatadir, "/aa/mouse/SMS+18_aa.csv.xz"))
   } else if(study=="DSNM16") {
     # 20200408 Anabaena circinalis, D'Agostino et al., 2016
     # DSNM16_131C, DSNM16_310F
@@ -236,19 +187,9 @@ pdat_osmotic <- function(dataset = 2020, basis = "rQEC") {
     dat <- dat[!is.na(dat[, icol]), ]
     up2 <- dat[, icol] > 1
     pcomp <- protcomp(dat$Entry, basis, aa_file = file.path(extdatadir, "aa/bacteria/GBR+20_aa.csv.xz"))
-  } else if(study=="LFY+12") {
-    # 20200417 HEK293 cells, Li et al., 2012
-    # LFY+12_C1h, LFY+12_C8h, LFY+12_C2p, LFY+12_N1h, LFY+12_N8h, LFY+12_N2p
-    dat <- read.csv(file.path(datadir, "LFY+12.csv.xz"), as.is=TRUE)
-    compartment <- ifelse(grepl("C", stage), "cytoplasm", "nucleus")
-    time <- substr(stage, 2, 2)
-    units <- ifelse(grepl("h", stage), "h", "passages")
-    description <- paste(compartment, "of HEK293 cells in 500 (NaCl added) vs 300 mosmol/kg medium for", time, units)
-    icol <- grep(stage, colnames(dat))
-    dat <- dat[!is.na(dat[, icol]), ]
-    dat <- check_IDs(dat, "UniProt.ID")
-    up2 <- dat[, icol] > 0
-    pcomp <- protcomp(dat$UniProt.ID, basis)
+  } else if(study %in% c("OBBH11", "CLG+15", "YDZ+15")) {
+    # 20200418 datasets from the 2017 compilation that have been moved to pdat_osmotic_euk.R
+    return(pdat_osmotic_euk(dataset, basis))
   } else stop(paste("osmotic dataset", dataset, "not available"))
   print(paste0("pdat_osmotic: ", description, " [", dataset, "]"))
   # use the up2 from the cleaned-up data, if it exists 20191120
---
+++ R/pdat_osmotic_euk.R
@@ -0,0 +1,87 @@
+# canprot/R/pdat_osmotic_euk.R
+# retrieve protein IDs for osmotic stress experiments in eukaryotes
+# 20160926 jmd
+# 20200418 eukaryotes extracted from pdat_osmotic.R
+
+pdat_osmotic_euk <- function(dataset = 2020, basis = "rQEC") {
+  if(identical(dataset, 2020)) {
+    return(c(
+             "LTH+11=microbial", "OBBH11",
+             "LFY+12_C1h", "LFY+12_C8h", "LFY+12_C2p", "LFY+12_N1h", "LFY+12_N8h", "LFY+12_N2p",
+             "CLG+15", "YDZ+15=microbial",
+             "JBG+18=microbial", "SMS+18_wt", "SMS+18_FGFR12.deficient"
+             ))
+  }
+  # remove tags
+  dataset <- strsplit(dataset, "=")[[1]][1]
+  # get study and stage/condition
+  study <- strsplit(dataset, "_")[[1]][1]
+  stage <- paste(strsplit(dataset, "_")[[1]][-1], collapse="_")
+  extdatadir <- system.file("extdata", package="canprot")
+  datadir <- paste0(extdatadir, "/expression/osmotic/")
+  if(study=="CLG+15") {
+    # 20160925 conjunctival epithelial cells, Chen et al., 2015
+    dat <- read.csv(paste0(datadir, "CLG+15.csv.xz"), as.is=TRUE)
+    description <- paste("human conjunctival epithelial cells in 380 or 480 mOsm vs 280 mOsm NaCl")
+    # use proteins that have same direction of change in both conditions
+    dat <- dat[(dat$T1 > 1 & dat$T2 > 1) | (dat$T1 < 1 & dat$T2 < 1), ]
+    pcomp <- protcomp(dat$accession..UniProtKB.Swiss.Prot., basis=basis)
+    up2 <- dat$T1 > 1
+  } else if(study=="OBBH11") {
+    # 20160925 adipose-derived stem cells, Oswald et al., 2011
+    dat <- read.csv(paste0(datadir, "OBBH11.csv.xz"), as.is=TRUE)
+    description <- "adipose-derived stem cells in 400 mOsm vs 300 mOsm NaCl"
+    dat <- check_IDs(dat, "Uniprot.Protein.Code")
+    pcomp <- protcomp(dat$Uniprot.Protein.Code, basis=basis)
+    up2 <- dat$Elucidator.Expression.Ratio..Treated.Control. > 1
+  } else if(study=="YDZ+15") {
+    # 20160926 Yarrowia lipolytica, Yang et al., 2015
+    dat <- read.csv(paste0(datadir, "YDZ+15.csv.xz"), as.is=TRUE)
+    description <- paste("Yarrowia lipolytica in 4.21 osmol/kg vs 3.17 osmol/kg NaCl")
+    up2 <- dat$Av..ratio..high.low. > 0
+    dat <- cleanup(dat, "Accession.No.", up2)
+    pcomp <- protcomp(substr(dat$Accession.No., 4, 12), basis=basis, aa_file=paste0(extdatadir, "/aa/yeast/YDZ+15_aa.csv.xz"))
+  } else if(study=="JBG+18") {
+    # 20200406 Candida albicans 1 M NaCl, Jacobsen et al., 2018
+    dat <- read.csv(paste0(datadir, "JBG+18.csv.xz"), as.is=TRUE)
+    description <- "Candida albicans in 1 M NaCl vs medium with no added NaCl"
+    up2 <- dat$log2.ratio > 0
+    dat <- cleanup(dat, "Entry", up2)
+    pcomp <- protcomp(dat$Entry, basis = basis, aa_file = paste0(extdatadir, "/aa/yeast/JBG+18_aa.csv.xz"))
+  } else if(study=="LTH+11") {
+    # 20200406 Saccharomyces cerevisae 0.7 M NaCl, Lee et al., 2011
+    dat <- read.csv(paste0(datadir, "LTH+11.csv.xz"), as.is=TRUE)
+    description <- "S. cerevisae in 0.7 M NaCl vs control medium (YPD)"
+    up2 <- dat$Regulation == "up"
+    pcomp <- protcomp(dat$Entry, basis, aa_file = paste0(extdatadir, "/aa/yeast/LTH+11_aa.csv.xz"))
+  } else if(study=="SMS+18") {
+    # 20200407 mouse skin in low/high humidity, Seltmann et al., 2018
+    # SMS+18_wt, SMS+18_FGFR12.deficient
+    dat <- read.csv(paste0(datadir, "SMS+18.csv.xz"), as.is=TRUE)
+    description <- paste("epidermal lysate of mice kept in 40% vs 70% humidity,", stage)
+    icol <- grep(stage, colnames(dat))
+    dat <- dat[abs(dat[, icol]) > 0.5, ]
+    dat <- check_IDs(dat, "Accession", aa_file = paste0(extdatadir, "/aa/mouse/SMS+18_aa.csv.xz"))
+    # abundance ratios are given as high humidity / low humidity, make up-expressed be low
+    up2 <- dat[, icol] < 0
+    dat <- cleanup(dat, "Accession", up2)
+    pcomp <- protcomp(dat$Accession, basis, aa_file = paste0(extdatadir, "/aa/mouse/SMS+18_aa.csv.xz"))
+  } else if(study=="LFY+12") {
+    # 20200417 HEK293 cells, Li et al., 2012
+    # LFY+12_C1h, LFY+12_C8h, LFY+12_C2p, LFY+12_N1h, LFY+12_N8h, LFY+12_N2p
+    dat <- read.csv(file.path(datadir, "LFY+12.csv.xz"), as.is=TRUE)
+    compartment <- ifelse(grepl("C", stage), "cytoplasm", "nucleus")
+    time <- substr(stage, 2, 2)
+    units <- ifelse(grepl("h", stage), "h", "passages")
+    description <- paste(compartment, "of HEK293 cells in 500 (NaCl added) vs 300 mosmol/kg medium for", time, units)
+    icol <- grep(stage, colnames(dat))
+    dat <- dat[!is.na(dat[, icol]), ]
+    dat <- check_IDs(dat, "UniProt.ID")
+    up2 <- dat[, icol] > 0
+    pcomp <- protcomp(dat$UniProt.ID, basis)
+  } else stop(paste("osmotic_euk dataset", dataset, "not available"))
+  print(paste0("pdat_osmotic_euk: ", description, " [", dataset, "]"))
+  # use the up2 from the cleaned-up data, if it exists 20191120
+  if("up2" %in% colnames(dat)) up2 <- dat$up2
+  return(list(dataset=dataset, basis=basis, pcomp=pcomp, up2=up2, description=description))
+}
