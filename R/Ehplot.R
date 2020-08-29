570321ef036fdc0b1c7bc3e42acb904df636d9e8
jedick <j3ffdick@gmail.com>
2020-08-29 14:22:25 +0700

    Update Ehplot()

--- R/Ehplot.R
+++ R/Ehplot.R
@@ -1,10 +1,10 @@
 # canprot/R/Ehplot.R
 # show values of Eh as a function of logaH2O/logfO2
 # 20160710 jmd
+# 20200829 add 'xlim' and 'ylim' arguments
 
-Ehplot <- function(basis="QEC", T=37, pH=7.4) {
-  logfO2 <- c(-75, -55)
-  H2O <- c(-10, 10)
+Ehplot <- function(T=37, pH=7.4, xlim = c(-75, -55), ylim = c(-10, 10), dy = 0.2) {
+  logfO2 <- xlim
   # logK for the reaction H2O(liq) = 2H+ + 2e- + 0.5O2(g)
   logK <- subcrt(c("H2O", "H+", "e-", "oxygen"), c(-1, 2, 2, 0.5), T=T)$out$logK
   # to calculate logaH2O at a given logfO2 and Eh
@@ -12,11 +12,12 @@ Ehplot <- function(basis="QEC", T=37, pH=7.4) {
     pe <- convert(Eh, "pe", T=convert(T, "K"))
     return(0.5*logfO2 - 2*pH - 2*pe - logK)
   }
-  plot(0, 0, xlim=logfO2, ylim=H2O, xlab=cplab$logfO2, ylab=cplab$logaH2O, type="n", xaxs="i", yaxs="i")
-  for(Eh in seq(-0.8, 0.2, by=0.2)) {
+  plot(0, 0, xlim=xlim, ylim=ylim, xlab=cplab$logfO2, ylab=cplab$logaH2O, type="n", xaxs="i", yaxs="i")
+  for(Eh in seq(-0.8, 0.2, by=dy)) {
     lines(logfO2, logaH2O(logfO2, Eh)) 
-    if(basis=="QEC") text(-61+19*Eh, logaH2O(-61+19*Eh, Eh) + 1, Eh)
-    if(basis=="CHNOS") text(-71+19*Eh, logaH2O(-71+19*Eh, Eh) + 1, Eh)
+    text(-61+19*Eh, logaH2O(-61+19*Eh, Eh) + 1, Eh)
   }
+  # add legend 20200829
+  legend("topright", describe.property(c("T", "pH"), c(T, pH), digits = 1), bg = "white")
   title(main="Eh (volt)", cex.main=1.1)
 }
